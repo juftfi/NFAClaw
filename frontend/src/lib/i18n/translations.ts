55e204e375bea885baac83893244074f98eade9c
juftfi <fourswapxyz@gmail.com>
2026-02-28 12:21:21 +0700

    Update translations.ts

--- frontend/src/lib/i18n/translations.ts
+++ frontend/src/lib/i18n/translations.ts
@@ -30,7 +30,7 @@ export const translations: Record<Locale, NestedStrings> = {
       wrongNetwork: 'Wrong Network',
     },
     hero: {
-      supply: '$NFAClaw : 7777',
+      supply: '$NFAClaw : 0x195075f504963810486037302abdcbae87867777',
       title: 'NFAClaw',
       description:
         'Wings to fly. Claws to hold. The first hybrid species on BSC Network.',
